9779c545926199ebeeae3a7d90c573762306a15c
Rokas <rookas.rudzenskas@gmail.com>
2023-12-19 13:07:24 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-// import CarouselBannerWrapper from "@/components/CarouselBannerWrapper";
-// import MoviesCarousel from "@/components/MoviesCarousel";
-// import {
-//   getPopularMovies,
-//   getTopRatedMovies,
-//   getUpcomingMovies,
-// } from "@/lib/getMovies";
+import {
+  getPopularMovies,
+  getTopRatedMovies,
+  getUpcomingMovies,
+} from "@/lib/getMovies";
 
 export default async function Home() {
-  // const upcomingMovies = await getUpcomingMovies();
-  // const topRatedMovies = await getTopRatedMovies();
-  // const popularMovies = await getPopularMovies();
+  const upcomingMovies = await getUpcomingMovies();
+  const topRatedMovies = await getTopRatedMovies();
+  const popularMovies = await getPopularMovies();
 
   return (
     <main className="">
